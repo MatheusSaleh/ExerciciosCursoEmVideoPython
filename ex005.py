20ea5c5ec363baf93c92a0f41123a980dd7261fe
MatheusSaleh <matheusxmg2@gmail.com>
2022-12-26 19:56:30 -0300

    Feito o exercicio 4,5,6

---
+++ ex005.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(numero, antecessor, sucessor))
---
+++ ex006.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raizQuadrada = numero ** 0.5
+print('O dobro de {} vale {}'.format(numero, dobro))
+print('O triplo de {} vale {}'.format(numero, triplo))
+print('A raiz quadrada de {} vale {}'.format(numero, round(raizQuadrada, 2)))
