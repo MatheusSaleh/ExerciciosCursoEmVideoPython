ae3cac4dd45221868f041b23fcaff5636691aa93
MatheusSaleh <matheusxmg2@gmail.com>
2022-12-26 19:30:40 -0300

    Feito o exercicio 3

---
+++ ex003.py
@@ -0,0 +1,4 @@
+primeiroNumero = int(input('Digite um valor '))
+segundoNumero = int(input('Digite outro valor '))
+resultado = primeiroNumero + segundoNumero
+print('A soma entre {} e {} é {}.'.format(primeiroNumero, segundoNumero, resultado))
